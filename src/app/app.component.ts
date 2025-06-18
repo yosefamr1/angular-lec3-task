95de1f6d5b5a5da57854729e2e9839b30838cc95
YoussefTrust <yosefamr2002@icloud.com>
2025-06-18 20:03:42 +0300

    project done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TemplateFormsComponent } from './template-forms/template-forms.component';
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TemplateFormsComponent,ReactiveFormsComponent],
+  imports: [RouterOutlet,TemplateFormsComponent,ReactiveFormsComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-lec3-task';
+  currentForm: 'template' | 'reactive' | null = null;
 }
