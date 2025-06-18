bbb8d29ee5ac7bf6c03162721fec6a8eba67380e
YoussefTrust <yosefamr2002@icloud.com>
2025-06-18 17:47:50 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TemplateFormsComponent } from './template-forms/template-forms.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TemplateFormsComponent,ReactiveFormsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
